e914903a1410fc235ff7477f918877372cd40a1c
logan0425 <72278371+logan0425@users.noreply.github.com>
2021-10-17 12:52:53 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<input id="fileupload" type="file" name="fileupload" />
+<button id="upload-button" onclick="uploadFile()"> host! </button>
+<script>
+async function uploadFile() {
+    let formData = new FormData();           
+    formData.append("file", fileupload.files[0]);
+    await fetch('/hostsrc.php', {
+      method: "POST", 
+      body: formData
+    });    
+    alert('The file has been uploaded successfully.');
+}
+</script>
